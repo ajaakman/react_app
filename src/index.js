7debb5c40bea10e8b73a7863a010fb3a9ef1e3eb
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-28 14:25:44 +0300

    Function Components.

--- src/index.js
+++ src/index.js
@@ -8,32 +8,31 @@ let skiData = {
     goal: 100
 }
 
-class SkiDayCounter extends Component {
-    getPercent = decimal => {
-        return decimal * 100 + '%'
-    }
-    calcGoalProgress = (total, goal) => {
-        return this.getPercent(total/goal)
-    }
-    render() {
-        const {total, powder, backcounty, goal} = this.props
-        return (
-            <section>
-                <div>
-                    <p> Total Days: {total}</p>
-                </div>
-                <div>
-                    <p> Powder Days: {powder}</p>
-                </div>
-                <div>
-                    <p> Backcounty Days: {backcounty}</p>
-                </div>
-                <div>
-                    <p> Goal Progress: {this.calcGoalProgress(total, goal)}</p>
-                </div>
-            </section>
-        )
-    }
+const getPercent = decimal => {
+    return decimal * 100 + '%'
+}
+
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total/goal)
+}
+
+const SkiDayCounter = ({total, powder,backcounty, goal}) =>{
+    return(
+    <section>
+        <div>
+            <p> Total Days: {total}</p>
+        </div>
+        <div>
+            <p> Powder Days: {powder}</p>
+        </div>
+        <div>
+            <p> Backcounty Days: {backcounty}</p>
+        </div>
+        <div>
+            <p> Goal Progress: {calcGoalProgress(total, goal)}</p>
+        </div>
+    </section>
+    )
 }
 
 ReactDOM.render(
