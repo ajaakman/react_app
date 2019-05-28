87874d0d47c13185bc4c9e779b534c3f6b72eb0a
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-28 14:08:38 +0300

    React Properties.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 
 class Message extends React.Component{
     render() {
+        console.log(this.props)
         return (
             <div>
-                <h1>Hello</h1>
+                <h1 style={{color: this.props.color}}>{this.props.msg}</h1>
+                <h1> I am {this.props.age} years old</h1>                
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <Message />,
+    <Message color="blue" age={27} msg="hello, how are you"/>,
     document.getElementById('root')
 )
