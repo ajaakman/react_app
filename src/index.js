3f38972d49a6cf6a1a9c3f2732475dc17411faab
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-29 12:59:50 +0300

    Changing Componnet State.

--- src/index.js
+++ src/index.js
@@ -18,17 +18,20 @@ const Book = ({title, author, pages}) => {
 }
 
 class Library extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            open: true
-        }
+    state = { open: false}
+    
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
     }
+
     render() {
         const { books } = this.props
         return (
             <div>
                 <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
                     (book, i) => <Book 
                                 key={i}
