9f3a1c097ab9252d375cd3859818b67bb73a6f83
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-29 14:08:33 +0300

    Fetching Data and Updating.

--- src/index.js
+++ src/index.js
@@ -33,16 +33,17 @@ class Library extends React.Component {
     state = { 
         open: true,
         freeBookmark: false,
-        hiring: false
+        hiring: false,
+        data: [],
+        loading: false
     }
 
     componentDidMount() {
-        console.log("Component is  mounted!!")
-    }
-    
-    componentDidUpdate() {
-        console.log("Component updated!!")        
-    }
+        this.setState({loading:true})
+        fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+            .then(data => data.json())
+            .then(data => this.setState({data, loading: false}))
+    }    
     
     toggleOpenClosed = () => {
         this.setState(prevState => ({
@@ -55,6 +56,20 @@ class Library extends React.Component {
         return (
             <div>
                 {this.state.hiring ? <Hiring /> : <NotHiring />}
+                {this.state.loading 
+                    ? "loading..." 
+                    : <div>
+                        {this.state.data.map(product => {
+                            return (
+                                <div>
+                                    <h3>Library prouct of the week</h3>
+                                    <h4>{product.name}</h4>
+                                    <img src={product.image} height={100}/>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
                 <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
                 <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
